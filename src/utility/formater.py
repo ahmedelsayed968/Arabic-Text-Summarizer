142d708351dd72e3795d25d14a7a28d6be30b04f
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-10-09 13:28:07 +0300

    [Style]: format the module

--- src/utility/formater.py
+++ src/utility/formater.py
@@ -1,12 +1,12 @@
 from typing import Dict
-def alpaca_input_format(entry:Dict[str,str]):
+
+
+def alpaca_input_format(entry: Dict[str, str]):
     instruction_text = (
-      f"فيما يلي تعليمات تصف مهمة ما. "
-      f"اكتب ردًا يكمل الطلب بشكل مناسب."
-      f"\n\n### تعليمات:\n{entry['instruction']}"
+        f"فيما يلي تعليمات تصف مهمة ما. "
+        f"اكتب ردًا يكمل الطلب بشكل مناسب."
+        f"\n\n### تعليمات:\n{entry['instruction']}"
     )
 
-    input_text = (
-        f"\n\n### النص:\n{entry['input']}" if entry["input"] else ""
-    )
+    input_text = f"\n\n### النص:\n{entry['input']}" if entry["input"] else ""
     return instruction_text + input_text
