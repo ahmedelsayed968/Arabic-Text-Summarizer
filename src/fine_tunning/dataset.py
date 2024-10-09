eb8cda5b41b2b3d8aa37bd80d676daab7dec2492
ahmedelsayed968 <ahmedeltop968@gmail.com>
2024-10-09 11:33:07 +0300

    [Feat]: add custom callate - handle masked instruction fine tunning - handle padding with max_length in batch strategy - handle allowed max length based on the used model

--- src/fine_tunning/dataset.py
+++ src/fine_tunning/dataset.py
@@ -1,7 +1,7 @@
 from torch.utils.data import Dataset,DataLoader
 from typing import Callable
 from datasets import Dataset as HFDataset
-# from tokenizers import Tokenizer
+import torch
 from transformers import AutoTokenizer
 class CustomInstructDataset(Dataset):
     def __init__(self,
@@ -22,4 +22,43 @@ class CustomInstructDataset(Dataset):
         return (self.tokenizer.encode(model_instruction),
                 self.tokenizer.encode(desired_response))
     
+
+def custom_callate(batch,
+                   device="cpu",
+                   ignore_index=-100,
+                   pad_token_id=50256,
+                   allowed_max_length=128,
+                   mask_instruct=False):
+    max_batch_len = max([len(entry[0])+len(entry[1])+1 for entry in batch])
+    inputs = []
+    targets = []
+    for instruct,response in batch:
+
+        padding = [pad_token_id]*(max_batch_len-(len(instruct)+len(response)))
+
+        full_seq = instruct+response+padding
+        input_pt = torch.tensor(full_seq)[:-1]
+        if mask_instruct:
+            masked_instruct = [ignore_index]*len(instruct[1:])
+            masked_instr_seq = masked_instruct+response+padding
+
+            target_pt = torch.tensor(masked_instr_seq)
+        else:
+            target_pt = torch.tensor(full_seq[1:])
+
+        mask = target_pt == pad_token_id
+        indices_to_mask = torch.nonzero(mask).squeeze()
+        if indices_to_mask.numel() > 1:
+            indices_skiped_first = indices_to_mask[1:]
+            target_pt[indices_skiped_first] = ignore_index
+
+
+        input_pt = input_pt[:allowed_max_length]
+        target_pt = target_pt[:allowed_max_length]
+
+        inputs.append(input_pt)
+        targets.append(target_pt)
+    res_inputs = torch.stack(inputs).to(device)
+    res_targets = torch.stack(targets).to(device)
+    return res_inputs,res_targets
 
